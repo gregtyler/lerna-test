720d10bfc2c06d0c00c7d60bc335065bedc01e87
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-06-01 09:49:59 +0100

    Fix search tests

--- packages/search/src/App.test.js
+++ packages/search/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders search invitation', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Search for a Pokemon/i);
   expect(linkElement).toBeInTheDocument();
 });
